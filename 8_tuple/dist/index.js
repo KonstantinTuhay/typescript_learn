42ed24e7cb8127be9c1a9f2f25afe0e9fadfd26d
Konstantin Tuhay <tugay-k@mail.ru>
2024-09-05 12:35:39 +0300

    tuple

---
+++ 8_tuple/dist/index.js
@@ -0,0 +1,16 @@
+"use strict";
+const pairs = [
+  ["key1", "value1"],
+  ["key2", "value2"],
+];
+console.log(pairs);
+///////////////////////////////////////
+const data = [
+  [2, true, "sfds"],
+  [1, false, "true"],
+];
+console.log(data);
+///////////////////////////////////////
+const doc = [];
+doc.push(["Kostya", "Tuhay", 24, new Date(2000, 4, 5)]);
+console.log(doc);
---
+++ 8_tuple/src/index.ts
@@ -0,0 +1,19 @@
+const pairs: [string, string][] = [
+  ["key1", "value1"],
+  ["key2", "value2"],
+];
+console.log(pairs);
+
+///////////////////////////////////////
+
+const data: [number, boolean, string][] = [
+  [2, true, "sfds"],
+  [1, false, "true"],
+];
+console.log(data);
+
+///////////////////////////////////////
+
+const doc: [string, string, number, Date][] = [];
+doc.push(["Kostya", "Tuhay", 24, new Date(2000, 4, 5)]);
+console.log(doc);
